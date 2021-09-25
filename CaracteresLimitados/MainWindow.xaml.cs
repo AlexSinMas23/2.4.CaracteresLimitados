8c595083ae046d55d6d285d66419cff8a8402265
AlexSinMas23 <ba.mota@iesdoctorbalmis.com>
2021-09-25 11:07:16 +0200

    quita espacios de nombre

--- CaracteresLimitados/MainWindow.xaml.cs
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CaracteresLimitados
 {
@@ -20,11 +9,7 @@ namespace CaracteresLimitados
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
+        public MainWindow() => InitializeComponent();
         private void cajaDeTextoLimitado_TextChanged(object sender, TextChangedEventArgs e)
         {
             this.contador.Text = this.cajaDeTextoLimitado.Text.Length.ToString();
@@ -39,10 +24,6 @@ namespace CaracteresLimitados
                 this.contador.FontSize = 12;
             }
         }
-
-        private void enterBoton_Click(object sender, RoutedEventArgs e)
-        {
-            this.cajaDeTextoLimitado.Text = "";
-        }
+        private void enterBoton_Click(object sender, RoutedEventArgs e) => this.cajaDeTextoLimitado.Text = "";
     }
 }
